d83b98ed44bdcd06cf9aa18ff7863139853d4f75
vaibhav <chandravaibhav65@gmail.com>
2023-05-15 19:08:37 +0530

    updated photo

--- src/components/College.js
+++ src/components/College.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import img from '../assets/logo.png'
 
-export default function College() {
+export default function College({title,line1,line2,line3}) {
     return (
         <div className="college">
+            <h2>{title}</h2>
             <div className="card" >
                 <div className="cbody" >
-                    <h2>Indian Institute of Technology, Indore</h2>
-                    <p>BTech, Computer Science and Engineering</p>
-                    <p>2019 - present</p>
+                    <h2>{line1}</h2>
+                    <p>{line2}</p>
+                    <p>{line3}</p>
                 </div>
             </div>
         </div >
--- src/pages/About.js
+++ src/pages/About.js
@@ -8,8 +8,10 @@ export default function About() {
             <h1>I'm Vaibhav Chandra</h1>
             <h3>Full Stack Developer | Casual Competetive Programmer</h3>
             <p className="aboutdesc">I always had a love for computers. It is growing even more during my undergrad in CSE. I enjoy making web applications completely from frontend to the backend and the database. MERN stack is my preference but I am always learning something new. With my knowledge I want to contribute to an organisation and grow myself even further.</p>
-            <h4>Position Of Responsibility: Member of College Web Team</h4>
-            <College />
+            <College title = 'Education' line1='Indian Institute of Technology, Indore' line2='BTech, Computer Science and Engineering' line3='2019 - 2023' />
+            <College title = 'Work Experience' line1='Software Development Intern' line2='Arcesium Pvt. Ltd., Hyderabad' line3='May 2022 - July 2022' />
+            <College title='' line1='College Web Team' line2='Indian Institute of Technology, Indore' line3='Oct. 2020 - Oct. 2021' />
+
         </div>
     )
 }
