221460bb9c6db38f723b030f96f2c42af2c60315
vaibhav <chandravaibhav65@gmail.com>
2023-05-28 02:26:56 +0530

    made navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,22 +26,38 @@ export default function Navbar() {
         console.log(id);
     }
     return (
-        <nav>
-            <ul>
-                <li onClick={() => activate('home')} >
+                <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <div class="container-fluid">
+        <a class="navbar-brand" href="#"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item" onClick={() => activate('home')} >
                     <Link exact to='/' id="home">Home</Link>
                 </li>
-                <li onClick={() => activate('about')}>
+                <li class="nav-item" onClick={() => activate('about')}>
                     <Link id="about" to='/about' >About</Link>
                 </li>
-                <li onClick={() => activate('skills')}>
+                <li class="nav-item" onClick={() => activate('skills')}>
                     <Link id="skills" to='/skills' >Skills</Link>
                 </li>
-                <li onClick={() => activate('projects')} >
+                <li class="nav-item" onClick={() => activate('projects')} >
                     <Link id="projects" to='/projects' >Projects</Link>
                 </li>
+                {/* <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li> */}
             </ul>
-
+            </div>
+        </div>
         </nav>
 
     )
